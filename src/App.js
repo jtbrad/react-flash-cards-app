bb5d1ef422f7f3eede2776d908c46cab518da662
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-12-03 15:50:09 -0700

    created the FlashCardForm component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import FlashCardForm from './FlashCardForm';
 import FlashCards from './FlashCards'
 import { Container, Header } from 'semantic-ui-react';
 
 class App extends React.Component {
   state = {
     flashCards: [
-      { id: 1, question: "question 1", answer: "answer 1" },
-      { id: 2, question: "question 2", answer: "answer 2" },
-      { id: 3, question: "question 3", answer: "answer 3" },
+      { id: 1, question: "question 1", answer: "answer 1", isFront: false, },
+      { id: 2, question: "question 2", answer: "answer 2", isFront: false, },
+      { id: 3, question: "question 3", answer: "answer 3", isFront: false, },
     ],
+  };
+
+  getId = () => Math.floor((1 + Math.random()) * 10000);
+
+  addFlashCard = (flashCardData) => {
+    let flashCard = { id: this.getId(), ...flashCardData, isFront: false, }
+    this.setState({ flashCards: [...this.state.flashCards, flashCard], })
   };
 
   render() {
@@ -16,6 +24,8 @@ class App extends React.Component {
     return (
       <Container>
         <Header as="h1">React Flash Cards</Header>
+        <br />
+        <FlashCardForm addFlashCard={this.addFlashCard}/>
         <br />
         <FlashCards flashCards={flashCards} />
       </Container>
---
+++ src/FlashCardForm.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Form, } from 'semantic-ui-react';
+
+class FlashCardForm extends React.Component {
+  state = { question: "", answer: "", };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value, });
+  };
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addFlashCard(this.state);
+    this.setState({ question: "", answer: "", });
+  };
+
+  render() {
+    return(
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Input
+          fluid
+          label={"Question"}
+          placeholder="Enter Question"
+          name="question"
+          value={this.state.queston}
+          onChange={this.handleChange}
+        />
+        <Form.Input 
+          fluid
+          label={"Answer"}
+          placeholder="Enter Answer"
+          name="answer"
+          value={this.state.answer}
+          onChange={this.handleChange}
+        />
+        <Form.Button>Submit</Form.Button>
+      </Form>
+    );
+  };
+};
+
+export default FlashCardForm;
