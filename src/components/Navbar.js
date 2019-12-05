4e67f870d02fdd823b6d40786a81d55e58f31734
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-12-05 16:27:51 -0700

    added home and about links to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link, } from 'react-router-dom';
+import { Menu, } from 'semantic-ui-react';
 
 const Navbar = () => (
-  <h1>Navbar</h1>
+  <Menu>
+    <Link to="/">
+      <Menu.Item>
+        Home
+      </Menu.Item>
+    </Link>
+    <Link to="/about">
+      <Menu.Item>
+        About
+      </Menu.Item>
+    </Link>
+  </Menu>
 );
 
 export default Navbar
