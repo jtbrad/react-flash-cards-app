4fb0071e3d9e7e568566e691677c5bb412bb42d3
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-12-05 16:24:07 -0700

    created a no match page and navbar stub

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import About from './components/About';
 import Home from './components/Home';
-import { Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import NoMatch from './components/NoMatch';
+import { Container } from 'semantic-ui-react';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <>
-    <Route exact path="/about" component={About}/>
-    <Route exact path="/" component={Home}/>
+    <Navbar />
+    <Container>
+      <Switch>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/" component={Home}/>
+        <Route component={NoMatch}/>
+      </Switch>
+    </Container>
   </>
 );
 
